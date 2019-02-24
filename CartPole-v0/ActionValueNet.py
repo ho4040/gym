61d5954f7f9678c78ad9c5d48f5fd334854f67b6
ho4040 <ho4040@nooslab.com>
2019-02-24 16:06:27 +0900

    cartpole mc updated

--- CartPole-v0/ActionValueNet.py
+++ CartPole-v0/ActionValueNet.py
@@ -3,21 +3,23 @@ import numpy as np
 
 class ActionValueNet:
 	"Q function approximatior"
-	def __init__(self, learning_rete=0.001):
+	def __init__(self, learning_rete=0.01):
 		self.build(learning_rete)
 
-	def build(self, learning_rete=0.001):
+	def build(self, learning_rete=0.01):
 		self.model = tf.keras.Sequential()
 		self.model.add(tf.keras.layers.Dense(units=128, activation='relu'))
 		self.model.add(tf.keras.layers.Dense(units=128, activation='relu'))
+		# self.model.add(tf.keras.layers.Dense(units=32, activation='relu'))
 		self.model.add(tf.keras.layers.Dense(units=1, activation=None))
-		self.model.compile(optimizer='adam', loss='mean_squared_error')
+		opt = tf.keras.optimizers.SGD(lr=learning_rete, decay=1e-4, momentum=0.9, nesterov=True)
+		self.model.compile(optimizer=opt, loss='mean_squared_error')
 
 	def train(self, samples, labels, epochs):
 		states = np.array([s['state'] for s in samples])
 		actions = np.array([[s['action']] for s in samples])
 		x = np.concatenate([states, actions], axis=1)
-		self.model.fit(x, np.array(labels), epochs=epochs, verbose=1)
+		return self.model.fit(x, np.array(labels), epochs=epochs, verbose=0)
 	
 	def predict(self, state, actions):
 		states = np.array([state.tolist()] * len(actions))
--- CartPole-v0/cartpole_MC_model_free.py
+++ CartPole-v0/cartpole_MC_model_free.py
@@ -4,43 +4,70 @@ import numpy as np
 import random
 
 env = gym.make("CartPole-v0")
-q = ActionValueNet()
+
+# gamma = 0.1
+# epsilon = 0.05
+# episode = 0
+# batch_size = 1
+# epoch = 10
+#learning_rate = 0.01
+
+gamma = 0.2
 epsilon = 0.05
 episode = 0
-batch_size = 100
+batch_size = 4
+epoch = 10
+learning_rate = 0.02
 
-for j in range(2000): 
-    samples = []
-    print("episode %d"%episode)
-        
-    for i in range(batch_size):
-        
-        episode += 1 
-        
-        trajectory = []
-        state = env.reset()
-        
-        while True:
-            if episode > 1000:
-                env.render()
-            action = env.action_space.sample()
-            
-            if random.random() > epsilon:
-                action_probs = q.predict(state, [0, 1])
-                action = np.argmax(action_probs)
-                
-            state, reward, terminated, info = env.step(action)
-            trajectory.append([state, action, reward])
-            if terminated :
-                break
-        
-        for t in range(len(trajectory)):
-            transition = trajectory[t]
-            state = transition[0]
-            action = transition[1] 
-            gt = sum([tr[2] for tr in trajectory[t:]])
-            samples.append([state, action, gt])
+q = ActionValueNet(learning_rate)
+qualified = False
+Tlist = []
 
-    states_action = np.array([ {"state":x[0], "action":x[1]} for x in samples])
-    gts = np.array([ x[2] for x in samples])
-    q.train(states_action, gts, 10)
+
+for j in range(100000): 
+	Tlist = []
+	samples = []
+	for i in range(batch_size):
+		episode += 1 
+		trajectory = []
+		state = env.reset()
+		while True:
+			if qualified:
+				env.render()
+			action = env.action_space.sample()
+			if random.random() > epsilon:
+				action_probs = q.predict(state, [0, 1])
+				action = np.argmax(action_probs)
+				
+			next_state, reward, terminated, info = env.step(action)
+			trajectory.append([state, action, reward])
+			state = next_state
+			if terminated :
+				break
+		
+		T = len(trajectory)
+		Tlist.append(T)
+
+		for t in range(T):
+			transition = trajectory[t]
+			s = transition[0]
+			a = transition[1] 
+			gt = sum([gamma**i * trajectory[t][2] for i in range(t, T)])
+			samples.append([s, a, gt])
+
+	avgT = np.mean(Tlist) if len(Tlist) > 0 else 0
+
+	if qualified == False:
+		qualified = True if avgT > 199 else False
+		states_action = np.array([ {"state":x[0], "action":x[1]} for x in samples])
+		gts = np.array([ x[2] for x in samples])
+		h = q.train(states_action, gts, epoch)
+		loss = np.mean(h.history['loss'])
+	else:
+		epsilon = 0
+		print("Qualified agent!")
+
+	epsilon = epsilon * 0.9999
+	print("#%d, avgT:%d, epsilon %f" % (episode, avgT, epsilon))
+	
+	
