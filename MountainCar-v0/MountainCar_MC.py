d9044fb7f7fda88e1b01be1f8c2ff01099b27190
ho4040 <ho4040@nooslab.com>
2019-02-25 00:27:35 +0900

    test

--- MountainCar-v0/MountainCar_MC.py
+++ MountainCar-v0/MountainCar_MC.py
@@ -7,9 +7,9 @@ env = gym.make("MountainCar-v0")
 all_actions = [0,1,2]
 
 # Hyper parameters
-gamma = 0.9
+gamma = 0.999
 epsilon = 0.99
-batch_size = 5
+batch_size = 1
 
 # init function approximator
 q = network.ActionValueNet(0.001, 0.0001, 10)
@@ -26,7 +26,7 @@ for j in range(20000):
 		state = env.reset()
 
 		# run episode
-		while True:
+		for t in range(1000):
 			action = env.action_space.sample()
 			if random.random() > epsilon:
 				action_probs = q.predict(state, all_actions)
@@ -42,9 +42,8 @@ for j in range(20000):
 		T = len(trajectory)
 
 		for t in range(T):  # Collect trainning datas
-			transition = trajectory[t]
-			s = transition[0]
-			a = transition[1]
+			s = trajectory[t][0]
+			a = trajectory[t][1]
 			gt = sum([(gamma**i) * trajectory[t+i][2] for i in range(0, T-t)])
 			samples.append([s, a, gt])
 
